835e94391095ede05e5411ccdb84fffb8ad272bc
Your Name <sbaird@sch.org>
2015-12-06 13:29:06 -0500

    RGB sliders after "passing date to the new view controller." Basically you can now see color choice in color square

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -11,24 +11,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+
+    
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
-    
     @IBOutlet weak var colorSquare: UIView!
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         colorSquare.layer.borderColor = UIColor.blackColor().CGColor
         colorSquare.layer.borderWidth = 1
         
-        
-        updateBackgroundColor()
-        
-        
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -40,10 +38,25 @@ class ViewController: UIViewController {
         let green = CGFloat(greenSlider.value)
         let blue = CGFloat(blueSlider.value)
 
-        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+       // view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
         
         colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+    
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "openColor") {
+            let newViewController = segue.destinationViewController
+            newViewController.view.backgroundColor = colorSquare.backgroundColor
+        }
+    }
+    
+
+    
 }
- //test comment
+
+
+
+
